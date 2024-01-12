c9099be6c42430a931c59eede0630619ae15d7d9
NikolayP <nikipanovski@gmail.com>
2024-01-12 12:50:23 +0100

    Init React project with React-Bootstrap (with sample horizontal card)

---
+++ portfolio-website/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import ImageAndTextExample from './components/ImageAndTextExample';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello World text.
+        </p>
+        <ImageAndTextExample />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio-website/src/components/ImageAndTextExample.js
@@ -0,0 +1,27 @@
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+function ImageAndTextExample() {
+  return (
+    <>
+      <Card className="d-flex">
+        <Row>
+          <Col xs={4}>  {/* out of 12 as the 100% */}
+            <Card.Img src="logo512.png" />
+          </Col>
+          <Col>
+            <Card.Body>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Col>
+        </Row>
+      </Card>
+    </>
+  );
+}
+
+export default ImageAndTextExample;
