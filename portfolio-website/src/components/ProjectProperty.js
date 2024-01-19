32d43c2f0fc5aef89b1bb6fd48cfa854b38f9b76
NikolayP <nikipanovski@gmail.com>
2024-01-19 12:22:34 +0100

    Add expanding buttons (for "engine" properties)

--- portfolio-website/src/components/ProjectProperty.js
+++ portfolio-website/src/components/ProjectProperty.js
@@ -1,12 +1,12 @@
-
+import styles from "components/ExpandingButton.css";
 
 function ProjectProperty({ icon, describerText, propertyValue }) {
     return (
         <>
-            <div className="d-flex flex-row">
-                {icon ?? <></>}
-                {describerText ? <p className="h4">{describerText}: {propertyValue}</p> : <></>}
-            </div>
+            <button className="expand-btn d-flex align-items-center flex-row m-2 p-3 rounded-pill" style={{ }}>
+                <span className="expand-btn-icon px-2">{icon ?? <></>}</span>
+                <span className="expand-btn-text">{describerText ? <p className="h4">{describerText}: {propertyValue}</p> : <></>}</span>
+            </button>
         </>
     );
 }
