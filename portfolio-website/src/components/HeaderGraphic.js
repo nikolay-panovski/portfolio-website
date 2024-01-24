8f9f052c45f7132b97c9a7606e64c8e8c948b60a
NikolayP <nikipanovski@gmail.com>
2024-01-24 23:00:27 +0100

    Add proper left/right paddings to header <p>s

--- portfolio-website/src/components/HeaderGraphic.js
+++ portfolio-website/src/components/HeaderGraphic.js
@@ -6,18 +6,18 @@ function HeaderGraphic() {
             <div className="d-flex" style={{position:"relative", width:"100%", height:"67vh"}}>
                 <div style={{position:"relative", width:"50%", height:"100%"}}>
                     {/*img x 4, grid */}
-                    <p className="h3 px-sm-3 px-md-5" style={{textAlign:"right", position:"absolute", width:"100%", top:"80%" }}>Video games</p>
+                    <p className="h3 pr-8" style={{textAlign:"right", position:"absolute", width:"100%", top:"80%" }}>Video games</p>
                 </div>
 
                 {/* middle: vertical line with image in center, on top of the regular flow */}
                 <div style={{height:"100%", position:"absolute", top:"0", left:"calc(50% - 0.25rem)", borderStyle:"solid", borderWidth:"0.25rem", borderColor:"#fff"}}></div>
                 <img src={Face} alt="Nikolay Panovski face" width="192" height="192" className="rounded-circle"
                     style={{position:"absolute", top:"33%", left:"50%", transform:"translate(-96px,-96px)", borderStyle:"solid", borderWidth:"0.5rem", borderColor:"#fff"}} />
-                <h1 style={{textAlign:"center", position:"absolute", width:"100%", top:"60%" }}>Nikolay Panovski</h1>
+                <h1 className="h1" style={{textAlign:"center", position:"absolute", width:"100%", top:"60%" }}>Nikolay Panovski</h1>
 
                 <div style={{position:"relative", width:"50%", height:"100%"}}>
                     {/*img x 4, grid */}
-                    <p className="h3 px-sm-3 px-md-5" style={{textAlign:"left", position:"absolute", width:"100%", top:"80%" }}>Interactive experiences</p>
+                    <p className="h3 pl-8" style={{textAlign:"left", position:"absolute", width:"100%", top:"80%" }}>Interactive experiences</p>
                 </div>
             </div>
 
