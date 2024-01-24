14422f867308c0f47ad74d38012a21b25eb2fc51
NikolayP <nikipanovski@gmail.com>
2024-01-24 23:00:00 +0100

    Replace Card component with Tailwind-styled flex divs

--- portfolio-website/src/components/ImageAndTextExample.js
+++ portfolio-website/src/components/ImageAndTextExample.js
@@ -1,8 +1,3 @@
-import { Card } from 'reactstrap';
-import { CardBody, CardTitle, CardText } from 'reactstrap';
-import { Col } from 'reactstrap';
-import { Row } from 'reactstrap';
-
 import ProjectProperty from './ProjectProperty';
 import ProjectPropertyExpandable from './ProjectPropertyExpandable';
 
@@ -13,29 +8,34 @@ import ProcessingLogo from "icons/processing-1024.png";
 function ImageAndTextExample() {
   return (
     <>
-      <Card className="d-flex" style={{margin: "1rem 15%"}}>
-        <Row>
-          <Col xs="4">  {/* out of 12 as the 100% */}
-            <div style={{width:"100%", height:"100%", backgroundImage:"url(logo512.png)", backgroundPosition:"center", backgroundSize:"cover"}} ></div>
-          </Col>
-          <Col>
-            <CardBody>
-              <CardTitle className="h3">IMT&S Unity Prototype</CardTitle>
-              <ProjectProperty icon={<UnityLogo width="36px" height="36px"/>} describerText="Duration" propertyValue="2 weeks"/>
-              <ProjectProperty icon={<UnityLogo width="36px" height="36px"/>} propertyValue="Individual"/>
-              <div className="d-flex flex-row flex-wrap justify-content-start">
-                <ProjectPropertyExpandable icon={<UnityLogo width="36px" height="36px"/>} propertyValue="Unity"/>
-                <ProjectPropertyExpandable icon={<CSLogo width="36px" height="36px"/>} propertyValue="C#"/>
-              </div>
-              
-              <CardText>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-              </CardText>
-            </CardBody>
-          </Col>
-        </Row>
-      </Card>
+      {/*Card*/}
+      <div className='flex flex-row my-4 mx-[15%]'>
+        {/*Col*/}
+        <div className='basis-1/3'>
+          <div className='w-full h-full bg-center bg-cover' style={{backgroundImage:"url(/logo512.png)"}}></div>
+        </div>
+        {/*Col*/}
+        <div className='basis-2/3'>
+          {/*CardBody*/}
+          <div className='flex flex-column py-2 px-4'>
+            {/*CardTitle*/}
+            <h3 className=''>IMT&S Unity Prototype</h3>
+
+            <ProjectProperty icon={<UnityLogo width="36px" height="36px"/>} describerText="Duration" propertyValue="2 weeks"/>
+            <ProjectProperty icon={<UnityLogo width="36px" height="36px"/>} propertyValue="Individual"/>
+            <div className="flex flex-row flex-wrap justify-start">
+              <ProjectPropertyExpandable icon={<UnityLogo width="36px" height="36px"/>} propertyValue="Unity"/>
+              <ProjectPropertyExpandable icon={<CSLogo width="36px" height="36px"/>} propertyValue="C#"/>
+            </div>
+
+            {/*CardText*/}
+            <p className=''>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
