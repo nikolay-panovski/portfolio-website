00a082f96563d169e79b46e616466b0623299213
NikolayP <nikipanovski@gmail.com>
2024-01-24 22:05:45 +0100

    CSS: Replace React Bootstrap with Reactstrap

--- portfolio-website/src/components/HeaderGraphic.js
+++ portfolio-website/src/components/HeaderGraphic.js
@@ -3,21 +3,21 @@ import Face from "photos/492686_square.jpg";
 function HeaderGraphic() {
     return (
         <>
-            <div className="d-flex" style={{position:"relative", width:"100%", height:"75vh"}}>
-                <div style={{width:"50%", height:"100%"}}>
-                    left{/*img x 4, grid */}
-                    {/* text: title 1 (games) */}
+            <div className="d-flex" style={{position:"relative", width:"100%", height:"67vh"}}>
+                <div style={{position:"relative", width:"50%", height:"100%"}}>
+                    {/*img x 4, grid */}
+                    <p className="h3 px-sm-3 px-md-5" style={{textAlign:"right", position:"absolute", width:"100%", top:"80%" }}>Video games</p>
                 </div>
 
                 {/* middle: vertical line with image in center, on top of the regular flow */}
                 <div style={{height:"100%", position:"absolute", top:"0", left:"calc(50% - 0.25rem)", borderStyle:"solid", borderWidth:"0.25rem", borderColor:"#fff"}}></div>
                 <img src={Face} alt="Nikolay Panovski face" width="192" height="192" className="rounded-circle"
                     style={{position:"absolute", top:"33%", left:"50%", transform:"translate(-96px,-96px)", borderStyle:"solid", borderWidth:"0.5rem", borderColor:"#fff"}} />
-                {/* text: my name */}
+                <h1 style={{textAlign:"center", position:"absolute", width:"100%", top:"60%" }}>Nikolay Panovski</h1>
 
-                <div style={{width:"50%", height:"100%"}}>
-                    right{/*img x 4, grid */}
-                    {/* text: title 2 (outdoors) */}
+                <div style={{position:"relative", width:"50%", height:"100%"}}>
+                    {/*img x 4, grid */}
+                    <p className="h3 px-sm-3 px-md-5" style={{textAlign:"left", position:"absolute", width:"100%", top:"80%" }}>Interactive experiences</p>
                 </div>
             </div>
 
--- portfolio-website/src/components/ImageAndTextExample.js
+++ portfolio-website/src/components/ImageAndTextExample.js
@@ -1,6 +1,7 @@
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
+import { Card } from 'reactstrap';
+import { CardBody, CardTitle, CardText } from 'reactstrap';
+import { Col } from 'reactstrap';
+import { Row } from 'reactstrap';
 
 import ProjectProperty from './ProjectProperty';
 import ProjectPropertyExpandable from './ProjectPropertyExpandable';
@@ -14,12 +15,12 @@ function ImageAndTextExample() {
     <>
       <Card className="d-flex" style={{margin: "1rem 15%"}}>
         <Row>
-          <Col xs={4}>  {/* out of 12 as the 100% */}
+          <Col xs="4">  {/* out of 12 as the 100% */}
             <div style={{width:"100%", height:"100%", backgroundImage:"url(logo512.png)", backgroundPosition:"center", backgroundSize:"cover"}} ></div>
           </Col>
           <Col>
-            <Card.Body>
-              <Card.Title as="div" className="h4">IMT&S Unity Prototype</Card.Title>
+            <CardBody>
+              <CardTitle className="h3">IMT&S Unity Prototype</CardTitle>
               <ProjectProperty icon={<UnityLogo width="36px" height="36px"/>} describerText="Duration" propertyValue="2 weeks"/>
               <ProjectProperty icon={<UnityLogo width="36px" height="36px"/>} propertyValue="Individual"/>
               <div className="d-flex flex-row flex-wrap justify-content-start">
@@ -27,11 +28,11 @@ function ImageAndTextExample() {
                 <ProjectPropertyExpandable icon={<CSLogo width="36px" height="36px"/>} propertyValue="C#"/>
               </div>
               
-              <Card.Text>
+              <CardText>
                 Some quick example text to build on the card title and make up the
                 bulk of the card's content.
-              </Card.Text>
-            </Card.Body>
+              </CardText>
+            </CardBody>
           </Col>
         </Row>
       </Card>
