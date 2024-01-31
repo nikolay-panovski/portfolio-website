242f56c67c73bb35bf405552770ef9448fa635ab
NikolayP <nikipanovski@gmail.com>
2024-01-31 12:20:21 +0100

    Add "external link" icons to project card titles

--- portfolio-website/src/components/ProjectCardTitle.js
+++ portfolio-website/src/components/ProjectCardTitle.js
@@ -1,9 +1,12 @@
-
+import { ReactComponent as LinkIcon } from "icons/icons8-external-link.svg";
 
 function ProjectCardTitle( { text } ) {
     return (
         <>
-            <h3 className='text-2xl font-bold'>{text}</h3>
+            <div className="flex flex-row flex-wrap items-start">
+                <h3 className='text-2xl font-bold'>{text}</h3>
+                <LinkIcon width="36px" height="36px" />
+            </div>
         </>
     );
 }
